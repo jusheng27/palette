02b5ca94d51f0b6b7bff720d332ee744488a5d5d
huang.jusheng <huang.jusheng@outlook.com>
2022-11-08 23:10:58 +0800

    feat: sidebar config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,14 @@
+const path = require('path'); // 路径模块
+const docsRoot = path.join(__dirname,'..');
+const readFile = require(path.join(__dirname, '/utils') + '/readFile');
+
 module.exports = {
   base: '/palette/',
   themeConfig: {
     logo: '/logo.png',
     nav: [
       {text: '日常', link: '/daily/'},
-      {text: '开发提效系列', link: '/efficiency/'},
+      {text: '开发提效系列', link: '/efficiency/01.Icon组件'},
       {text: 'element-plus', link: '/element-plus/'},
       {text: 'lodash', link: '/lodash/'},
       {text: '工程化', link: '/engineering/'},
@@ -17,6 +21,16 @@ module.exports = {
         ]
       },
       {text: '算法', link: '/algorithm/'},
-    ]
+    ],
+    displayAllHeaders: false,
+    docsDir: 'docs',
+    smoothScroll: true,
+    sidebar: {
+      '/efficiency/': [{
+        title: '开发提效系列',
+        collapsable: false,
+        children: readFile(docsRoot + '/efficiency')
+      }],
+    },
   }
 }

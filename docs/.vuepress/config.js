b50858990d5297debf76b80a2982059a47b87af5
huang.jusheng <huang.jusheng@outlook.com>
2022-11-10 10:32:49 +0800

    feat: add favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,36 +1,43 @@
 const path = require('path'); // 路径模块
-const docsRoot = path.join(__dirname,'..');
+const docsRoot = path.join(__dirname, '..');
 const readFile = require(path.join(__dirname, '/utils') + '/readFile');
 
 module.exports = {
   base: '/palette/',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  title: 'palette',
   themeConfig: {
     logo: '/logo.png',
+    docsDir: 'docs',
+    displayAllHeaders: false,
+    smoothScroll: true,
+
     nav: [
-      {text: '日常', link: '/daily/'},
-      {text: '开发提效系列', link: '/efficiency/01.Icon组件'},
-      {text: 'element-plus', link: '/element-plus/'},
-      {text: 'lodash', link: '/lodash/'},
-      {text: '工程化', link: '/engineering/'},
-      {text: '设计模式', link: '/design-patterns/'},
+      { text: '日常', link: '/daily/' },
+      { text: '开发提效系列', link: '/efficiency/01.Icon组件' },
+      { text: 'element-plus', link: '/element-plus/基础组件/01.Layout 布局' },
+      { text: 'lodash', link: '/lodash/' },
+      { text: '工程化', link: '/engineering/' },
+      { text: '设计模式', link: '/design-patterns/' },
       {
         text: '书籍笔记',
         items: [
-          {text: '《Vue.js 设计与实现》', link: '/book-notes/vue-design/'},
-          {text: '《JavaScript 设计模式与实践》', link: '/book-notes/js-design-patterns/'}
+          { text: '《Vue.js 设计与实现》', link: '/book-notes/vue-design/' },
+          { text: '《JavaScript 设计模式与实践》', link: '/book-notes/js-design-patterns/' }
         ]
       },
-      {text: '算法', link: '/algorithm/'},
+      { text: '算法', link: '/algorithm/' },
     ],
-    displayAllHeaders: false,
-    docsDir: 'docs',
-    smoothScroll: true,
+
     sidebar: {
       '/efficiency/': [{
         title: '开发提效系列',
         collapsable: false,
         children: readFile(docsRoot + '/efficiency')
       }],
+      '/element-plus/': readFile(docsRoot + '/element-plus')
     },
   }
 }
